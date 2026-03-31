673f10349daddb971d0633318375d24f76f5b60e
Boddupally_Saikrishna <boddupallysaikrishna6@gmail.com>
2026-03-31 09:54:08 +0530

    Update supabase.ts

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,8 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
-  || 'https://otjruslxfermmcnrnvbn.supabase.co';
-const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY
-  || 'sb_publishable_xnrpHOY4xNywaE-9ZXgFIA_mctU-FZb';
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
